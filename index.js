6d8309248c0f37b65168c687473c0fa4a85768e3
Cleber <cleber@4beerdev.com.br>
2023-11-15 15:43:27 -0300

    justesa

---
+++ index.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+//     // Esconde todos os submenus e opções inicialmente
+//     $('.dropdown-submenu, .dropdown-options').hide();
+  
+//     // Adiciona o comportamento de clique nas categorias
+//     $('.dropdown-categoria').click(function() {
+//       // Esconde todos os submenus e opções
+//       $('.dropdown-submenu, .dropdown-options').hide();
+  
+//       // Mostra o submenu correspondente à categoria clicada
+//       $(this).find('.dropdown-submenu').slideDown('fast');
+//     });
+  
+//     // Adiciona o comportamento de clique nas subcategorias
+//     $('.dropdown-subcategoria').click(function() {
+//       // Esconde todos os submenus e opções
+//       $('.dropdown-submenu, .dropdown-options').hide();
+  
+//       // Mostra as opções correspondentes à subcategoria clicada
+//       $(this).find('.dropdown-options').slideDown('fast');
+//     });
+//   });
+  
